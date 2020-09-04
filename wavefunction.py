0cac678a7571c30e948f4406f04d2123d314547a
afouda11 <adam_540@hotmail.co.uk>
2020-09-04 14:49:40 +0100

    large restructure and write

--- wavefunction.py
+++ wavefunction.py
@@ -4,30 +4,33 @@ from kshelper import diag_H,Timer, ACDIIS,printHeader
 import time
 
 mol = psi4.geometry("""
-  C    1.4250724    0.0644040   -0.1137090
-  H    2.5063157    0.1844071   -0.1159750
-  C    0.7262282    0.1039660   -1.2602414
-  H    1.2272923    0.2717911   -2.2110146
-  C   -0.7262282   -0.1039660   -1.2602414
-  H   -1.2272923   -0.2717911   -2.2110146
-  C   -1.4250724   -0.0644040   -0.1137090
-  H   -2.5063157   -0.1844071   -0.1159750
-  C   -0.7318022    0.2394181    1.1953283
-  H   -0.7633292    1.3299694    1.3618674
-  H   -1.2721104   -0.2098681    2.0368776
-  C    0.7318022   -0.2394181    1.1953283
-  H    0.7633292   -1.3299694    1.3618674
-  H    1.2721104    0.2098681    2.0368776
+
+        C         1.4225362665    0.0668177861   -0.1118069873
+        H         2.5020006041    0.1902163842   -0.1153665161
+        C         0.7248577450    0.1081828626   -1.2567194972
+        H         1.2268155727    0.2809839700   -2.2045192821
+        C        -0.7248577450   -0.1081828626   -1.2567194972
+        H        -1.2268155727   -0.2809839700   -2.2045192821
+        C        -1.4225362665   -0.0668177861   -0.1118069873
+        H        -2.5020006040   -0.1902163843   -0.1153665161
+        C        -0.7289141146    0.2427583297    1.1936049221
+        H        -0.7560498048    1.3329770153    1.3508934782
+        H        -1.2710307622   -0.1951779008    2.0370471334
+        C         0.7289141146   -0.2427583297    1.1936049221
+        H         0.7560498048   -1.3329770151    1.3508934782
+        H         1.2710307621    0.1951779007    2.0370471332
 symmetry C1
 noreorient
 nocom
 """)
 psi4.set_options({'basis':        '6-311+G*',
-                  'scf_type':     'pk',
+                  'scf_type':     'df',
                   'reference':    'uhf',
                   'mp2_type':     'conv',
                   'e_convergence': 1e-8,
-                  'd_convergence': 1e-8})
+                  'd_convergence': 1e-8,
+                  'PK_MAX_BUCKETS': 1100
+                  })
 
 options = {
     "E_CONV"   : 1.0E-6,
@@ -40,15 +43,21 @@ options = {
     "DIIS_MODE": "ADIIS+CDIIS",
     "MIXMODE"  : "DAMP",
     "LOC_SUB"  : [0, 1, 2, 3, 4, 5]}
-
-orbs   = [5]
+psi4.set_num_threads(8)
+orbs   = [3]
 occs   = [0.0]
 freeze = ["T"]
 spin   = ["b"] 
 ovl    = ["T"]
+dft    = "F"
+func   = 'b3lyp'
 # Get the SCF wavefunction & energies
-scf_e, scf_wfn = psi4.energy('scf', return_wfn=True)
+if dft == "F":
+    scf_e, scf_wfn = psi4.energy('scf', return_wfn=True)
+if dft == "T":
+    scf_e, scf_wfn = psi4.energy(func, return_wfn=True)
 
+aux    = psi4.core.BasisSet.build(mol, "DF_BASIS_SCF", "", "JKFIT", psi4.core.get_global_option('BASIS'))
 nalpha = scf_wfn.nalpha()
 nbeta  = scf_wfn.nbeta()
 nbf    = scf_wfn.nso()
@@ -73,10 +82,40 @@ occa = np.asarray(scf_wfn.occupation_a())
 occb = np.asarray(scf_wfn.occupation_b())
 
 mw = psi4.core.MoldenWriter(scf_wfn)
-mw.write('Neutral.molden',scf_wfn.Ca(),scf_wfn.Cb(),scf_wfn.epsilon_a(),scf_wfn.epsilon_b(),OCCA,OCCB,True)
+
+if dft == "F":
+    mw.write('hf_ground.molden',scf_wfn.Ca(),scf_wfn.Cb(),scf_wfn.epsilon_a(),scf_wfn.epsilon_b(),OCCA,OCCB,True)
+    np.savez('hf_gsorbs',Ca=Ca,Cb=Cb,occa=occa,occb=occb,epsa=epsa,epsb=epsb)
+
+if dft == "T":
+    mw.write('dft_grround.molden',scf_wfn.Ca(),scf_wfn.Cb(),scf_wfn.epsilon_a(),scf_wfn.epsilon_b(),OCCA,OCCB,True)
+    np.savez('dft_gsorbs',Ca=Ca,Cb=Cb,occa=occa,occb=occb,epsa=epsa,epsb=epsb)
+
+
 
 #LOCALISATION
 loc_sub = np.array(options["LOC_SUB"],dtype=np.int)
+
+if dft == "T":
+    sup = psi4.driver.dft.build_superfunctional(func, False)[0]
+    sup.set_deriv(2)
+    sup.allocate()
+    uhf   = psi4.core.UHF(scf_wfn,sup)
+    Ca = np.load("dft_gsorbs.npz")["Ca"]
+    Cb = np.load("dft_gsorbs.npz")["Cb"]
+    occa = np.load("dft_gsorbs.npz")["occa"]
+    occb = np.load("dft_gsorbs.npz")["occb"]
+    epsa = np.load("dft_gsorbs.npz")["epsa"]
+    epsb = np.load("dft_gsorbs.npz")["epsb"]
+
+if dft == "F":
+    Ca = np.load("hf_gsorbs.npz")["Ca"]
+    Cb = np.load("hf_gsorbs.npz")["Cb"]
+    occa = np.load("hf_gsorbs.npz")["occa"]
+    occb = np.load("hf_gsorbs.npz")["occb"]
+    epsa = np.load("hf_gsorbs.npz")["epsa"]
+    epsb = np.load("hf_gsorbs.npz")["epsb"]
+
 locCa = psi4.core.Matrix(scf_wfn.nso(),len(loc_sub))
 locCb = psi4.core.Matrix(scf_wfn.nso(),len(loc_sub))
 
@@ -92,22 +131,44 @@ LocalB.localize()
 Ca[:,loc_sub] = LocalA.L
 Cb[:,loc_sub] = LocalB.L
 
+if dft == "T":
+    np.savez('dft_gsorbs',Ca=Ca,Cb=Cb,occa=occa,occb=occb)
+    psi4.core.print_out("Localized Orbitals written\n")
+
+if dft == "F":
+    np.savez('hf_gsorbs',Ca=Ca,Cb=Cb,occa=occa,occb=occb)
+    psi4.core.print_out("Localized Orbitals written\n")
+
 OCCA = psi4.core.Vector(nbf)
 OCCB = psi4.core.Vector(nbf)
 OCCA.np[:] = occa
 OCCB.np[:] = occb
 
-Ca = np.asarray(scf_wfn.Ca())
-Cb = np.asarray(scf_wfn.Cb())
+if dft == "F":
+    Ca = np.asarray(scf_wfn.Ca())
+    Cb = np.asarray(scf_wfn.Cb())
 
-epsa = np.asarray(scf_wfn.epsilon_a())
-epsb = np.asarray(scf_wfn.epsilon_b())
+    epsa = np.asarray(scf_wfn.epsilon_a())
+    epsb = np.asarray(scf_wfn.epsilon_b())
 
-occa = np.asarray(scf_wfn.occupation_a())
-occb = np.asarray(scf_wfn.occupation_b())
+    occa = np.asarray(scf_wfn.occupation_a())
+    occb = np.asarray(scf_wfn.occupation_b())
 
-mw = psi4.core.MoldenWriter(scf_wfn)
-mw.write('Neutral_loc.molden',scf_wfn.Ca(),scf_wfn.Cb(),scf_wfn.epsilon_a(),scf_wfn.epsilon_b(),OCCA,OCCB,True)
+    mw = psi4.core.MoldenWriter(scf_wfn)
+    mw.write('hf_ground_loc.molden',scf_wfn.Ca(),scf_wfn.Cb(),scf_wfn.epsilon_a(),scf_wfn.epsilon_b(),OCCA,OCCB,True)
+
+if dft == "T":
+    uhf.Ca().np[:] = Ca
+    uhf.Cb().np[:] = Cb
+
+    uhf.epsilon_a().np[:] = epsa
+    uhf.epsilon_b().np[:] = epsb
+
+    uhf.occupation_a().np[:] = occa
+    uhf.occupation_b().np[:] = occb
+
+    mw = psi4.core.MoldenWriter(uhf)
+    mw.write('dft_ground_loc.molden',uhf.Ca(),uhf.Cb(),uhf.epsilon_a(),uhf.epsilon_b(),OCCA,OCCB,True)
 
 #CORE IONISATION
 
@@ -119,8 +180,13 @@ if  len(list((set(lens))))>1:
 for i in range(len(orbs)):
     orbitals.append({"orb" : orbs[i],"spin": spin[i].lower(),"occ" : occs[i], "frz" : freeze[i]=="T","DoOvl":ovl[i] == "T" })
 
-#Ca = np.load("gstate_sorbs.npz")["Ca"]
-#Cb = np.load("gstate_sorbs.npz")["Cb"]
+if dft == "T":
+    Ca = np.load("dft_gsorbs.npz")["Ca"]
+    Cb = np.load("dft_gsorbs.npz")["Cb"]
+
+if dft == "F":
+    Ca = np.load("hf_gsorbs.npz")["Ca"]
+    Cb = np.load("hf_gsorbs.npz")["Cb"]
 
 for i in orbitals:
     if i["spin"]=="b":
@@ -130,29 +196,51 @@ for i in orbitals:
     else:
         raise Exception("Orbital has non a/b spin!")
 
-mints = psi4.core.MintsHelper(scf_wfn.basisset())
+
+
+wfn    = psi4.core.Wavefunction.build(mol, psi4.core.get_global_option('basis'))
+aux     = psi4.core.BasisSet.build(mol, "DF_BASIS_SCF", "", "JKFIT", psi4.core.get_global_option('BASIS'))
+mints = psi4.core.MintsHelper(wfn.basisset())
 S = np.asarray(mints.ao_overlap())
 T = np.asarray(mints.ao_kinetic())
 V = np.asarray(mints.ao_potential())
 H = np.zeros((mints.nbf(),mints.nbf()))
 
 H = T+V
+if wfn.basisset().has_ECP():
+    ECP = mints.ao_ecp()
+    H += ECP
+
 A = mints.ao_overlap()
 A.power(-0.5,1.e-16)
 A = np.asarray(A)
 
 Enuc = mol.nuclear_repulsion_energy()
-Eold = 0.0
-SCF_E = 100.0
+Eold  =  0.0
+SCF_E = 0.0
+
+if dft == "T":
+    Va = psi4.core.Matrix(nbf,nbf)
+    Vb = psi4.core.Matrix(nbf,nbf)
+    sup = psi4.driver.dft.build_superfunctional(func, False)[0]
+    sup.allocate()
+
+    Vpot = psi4.core.VBase.build(wfn.basisset(), sup, "UV")
+    Vpot.initialize()
+
+    uhf   = psi4.core.UHF(wfn,sup)
+    Da_m = psi4.core.Matrix(nbf,nbf)
+    Db_m = psi4.core.Matrix(nbf,nbf)
 
 occa = np.zeros(Ca.shape[0])
 occb = np.zeros(Cb.shape[0])
 occa[:nalpha] = 1
-occa[:nbeta]  = 1
+occb[:nbeta]  = 1
 Cocca = psi4.core.Matrix(nbf, nbf)
 Coccb = psi4.core.Matrix(nbf, nbf)
 Cocca.np[:]  = Ca
 Coccb.np[:]  = Cb
+
 #initial density
 for i in orbitals:
     if i["spin"]=="b":
@@ -166,22 +254,60 @@ for i in orbitals:
         """
         i["ovl"] = np.max(ovl)
         occb[i["orb"]] = i["occ"]
-
 for i in range(nbf):
     Cocca.np[:,i] *= np.sqrt(occa[i])
     Coccb.np[:,i] *= np.sqrt(occb[i])
 
-Da = Cocca.np @ Cocca.np.T
-Db = Coccb.np @ Coccb.np.T
-jk = psi4.core.JK.build(scf_wfn.basisset(),jk_type=psi4.core.get_local_option("SCF","SCF_TYPE"))
+Da     = Cocca.np @ Cocca.np.T
+Db     = Coccb.np @ Coccb.np.T
+
+jk = psi4.core.JK.build(wfn.basisset(),aux,jk_type=psi4.core.get_local_option("SCF","SCF_TYPE"))
 glob_mem = psi4.core.get_memory()/8
 jk.set_memory(int(glob_mem*0.6))
+if dft == "T":
+    if (sup.is_x_hybrid()):
+        jk.set_do_K(True)
+    if (sup.is_x_lrc()):
+         jk.set_omega(sup.x_omega())
+         jk.set_do_wK(True)
 jk.initialize()
 jk.C_left_add(Cocca)
 jk.C_left_add(Coccb)
 
-Da_m = psi4.core.Matrix(nbf,nbf)
-Db_m = psi4.core.Matrix(nbf,nbf)
+jk.compute()
+Ja = np.asarray(jk.J()[0])
+Jb = np.asarray(jk.J()[1])
+Ka = np.asarray(jk.K()[0])
+Kb = np.asarray(jk.K()[1])
+one_electron_E = np.einsum('pq,pq->', (Da + Db), H, optimize=True)
+
+if dft == "F":
+    two_electron_E  = 0.5 * np.sum(Da * (Ja+Jb -Ka))
+    two_electron_E += 0.5 * np.sum(Db * (Ja+Jb -Kb))
+if dft == "T":
+    Da_m.np[:] = Da
+    Db_m.np[:] = Db
+    Vpot.set_D([Da_m,Db_m])
+    Vpot.compute_V([Va,Vb])
+
+    coulomb_E       = np.sum(Da * (Ja+Jb))
+    coulomb_E      += np.sum(Db * (Ja+Jb))
+    exchange_E  = 0.0
+    if sup.is_x_hybrid():
+        exchange_E -=  sup.x_alpha() * np.sum(Da * Ka)
+        exchange_E -=  sup.x_alpha() * np.sum(Da * Kb)
+    if sup.is_x_lrc():
+        exchange_E -= sup.x_beta() * np.sum(Da * np.asarray(jk.wK()[0]))
+        exchange_E -= sup.x_beta() * np.sum(Db * np.asarray(jk.wK()[1]))
+    XC_E = Vpot.quadrature_values()["FUNCTIONAL"]
+
+    two_electron_E = 0.5 * (coulomb_E + exchange_E) + XC_E
+
+SCF_E += Enuc + one_electron_E + two_electron_E
+
+psi4.core.print_out("{:>20} {:12.8f} [Ha] \n".format("One-Electron Energy:",one_electron_E))
+psi4.core.print_out("{:>20} {:12.8f} [Ha] \n".format("Two-Electron Energy:",two_electron_E))
+psi4.core.print_out("{:>20} {:12.8f} [Ha] \n".format("Total Guess Energy:",SCF_E))
 
 diis = ACDIIS(max_vec=options["DIIS_LEN"])
 printHeader("Starting SCF:",2)
@@ -223,20 +349,36 @@ for SCF_ITER in range(1, options["MAXITER"]+ 1):
     myTimer.addEnd("JK")
 
     myTimer.addStart("buildFock")
-    Da_m.np[:] = Da
-    Db_m.np[:] = Db
 
     Ja = np.asarray(jk.J()[0])
     Jb = np.asarray(jk.J()[1])
+
     Ka = np.asarray(jk.K()[0])
     Kb = np.asarray(jk.K()[1])
     
+    if dft == "T":
+        Da_m.np[:] = Da
+        Db_m.np[:] = Db
+        Vpot.set_D([Da_m,Db_m])
+        Vpot.compute_V([Va,Vb])
+
     if SCF_ITER>1 :
         FaOld = np.copy(Fa)
         FbOld = np.copy(Fb)
 
-    Fa = H +  (Ja + Jb) - Ka
-    Fb = H +  (Ja + Jb) - Kb
+    if dft == "F":
+        Fa = H +  (Ja + Jb) - Ka
+        Fb = H +  (Ja + Jb) - Kb
+
+    if dft == "T":
+        Fa = H + (Ja + Jb) + Va
+        Fb = H + (Ja + Jb) + Vb
+        if sup.is_x_hybrid():
+             Fa -= sup.x_alpha()* Ka
+             Fb -= sup.x_alpha()* Kb
+        if sup.is_x_lrc():
+            Fa -= sup.x_beta()*np.asarray(jk.wK()[0])
+            Fb -= sup.x_beta()*np.asarray(jk.wK()[1])
 
     myTimer.addEnd("buildFock")
 
@@ -293,15 +435,32 @@ for SCF_ITER in range(1, options["MAXITER"]+ 1):
 
     myTimer.addStart("calcE")
 
-    one_electron_E   = np.sum(Da * H)
-    one_electron_E  += np.sum(Db * H)
-    two_electron_E   = np.sum(Da * (Ja+Jb-Ka))
-    two_electron_E  += np.sum(Db * (Ja+Jb-Kb))
+    one_electron_E = np.einsum('pq,pq->', (Da + Db), H, optimize=True)
 
-    SCF_E  = 0.0
-    SCF_E += Enuc
-    SCF_E += one_electron_E
-    SCF_E += 0.5 * two_electron_E
+    if dft == "F":
+        two_electron_E  = 0.5 * np.sum(Da * (Ja+Jb -Ka))
+        two_electron_E += 0.5 * np.sum(Db * (Ja+Jb -Kb))
+    if dft == "T":
+        Da_m.np[:] = Da
+        Db_m.np[:] = Db
+        Vpot.set_D([Da_m,Db_m])
+        Vpot.compute_V([Va,Vb])
+
+        coulomb_E       = np.sum(Da * (Ja+Jb))
+        coulomb_E      += np.sum(Db * (Ja+Jb))
+        exchange_E  = 0.0
+        if sup.is_x_hybrid():
+            exchange_E -=  sup.x_alpha() * np.sum(Da * Ka)
+            exchange_E -=  sup.x_alpha() * np.sum(Da * Kb)
+        if sup.is_x_lrc():
+            exchange_E -= sup.x_beta() * np.sum(Da * np.asarray(jk.wK()[0]))
+            exchange_E -= sup.x_beta() * np.sum(Db * np.asarray(jk.wK()[1]))
+        XC_E = Vpot.quadrature_values()["FUNCTIONAL"]
+
+        two_electron_E = 0.5 * (coulomb_E + exchange_E) + XC_E
+    SCF_E = 0.0
+    SCF_E += Enuc + one_electron_E + two_electron_E
+
     myTimer.addEnd("calcE")
 
     myTimer.addStart("Diag")
@@ -430,11 +589,28 @@ OCCA = psi4.core.Vector(nbf)
 OCCB = psi4.core.Vector(nbf)
 OCCA.np[:] = occa
 OCCB.np[:] = occb
-scf_wfn.Ca().np[:] = Ca
-scf_wfn.Cb().np[:] = Cb
-scf_wfn.epsilon_a().np[:] = epsa
-scf_wfn.epsilon_b().np[:] = epsb
-scf_wfn.occupation_a().np[:] = occa
-scf_wfn.occupation_b().np[:] = occb
-mw = psi4.core.MoldenWriter(scf_wfn)
-mw.write('core_hole.molden',scf_wfn.Ca(),scf_wfn.Cb(),scf_wfn.epsilon_a(),scf_wfn.epsilon_b(),OCCA,OCCB,True)
+if dft == "F":
+    scf_wfn.Ca().np[:] = Ca
+    scf_wfn.Cb().np[:] = Cb
+    scf_wfn.epsilon_a().np[:] = epsa
+    scf_wfn.epsilon_b().np[:] = epsb
+    scf_wfn.occupation_a().np[:] = occa
+    scf_wfn.occupation_b().np[:] = occb
+    mw = psi4.core.MoldenWriter(scf_wfn)
+    mw.write('hf_core_hole.molden',scf_wfn.Ca(),scf_wfn.Cb(),scf_wfn.epsilon_a(),scf_wfn.epsilon_b(),OCCA,OCCB,True)
+if dft == "T":
+    uhf.Ca().np[:] = Ca
+    uhf.Cb().np[:] = Cb
+    uhf.epsilon_a().np[:] = epsa
+    uhf.epsilon_b().np[:] = epsb
+    uhf.occupation_a().np[:] = occa
+    uhf.occupation_b().np[:] = occb
+    mw = psi4.core.MoldenWriter(uhf)
+    mw.write('dft_core_hole.molden',uhf.Ca(),uhf.Cb(),uhf.epsilon_a(),uhf.epsilon_b(),OCCA,OCCB,True)
+
+
+
+
+
+
+
